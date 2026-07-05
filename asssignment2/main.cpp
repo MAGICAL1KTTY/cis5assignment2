0c56c36f333915ccfc9f423f78f8799526e0815b
MAGICAL1KTTY <181618833+MAGICAL1KTTY@users.noreply.github.com>
2026-07-05 00:30:46 -0700

    starting
    defined all required variables as well as input

---
+++ asssignment2/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	string name;
+	int age;
+	double height;
+	double weight;
+	int testScore;
+
+	cout << "Name? ";
+	cin >> name;
+	cout << endl;
+
+	cout << "Age? ";
+	cin >> age;
+	cout << endl;
+
+	cout << "Height in meters? ";
+	cin >> height;
+	cout << endl;
+
+	cout << "Weight in kilograms? ";
+	cin >> weight;
+	cout << endl;
+
+	cout << "Test score? ";
+	cin >> testScore;
+	cout << endl;
+
+
+}
